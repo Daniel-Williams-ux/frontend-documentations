35cd6f9efe9a2b09437bbaebcf0c2e9a5b1cb4d6
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-08 15:55:21 +0100

    Update button_function.jsx

--- react/button_function.jsx
+++ react/button_function.jsx
@@ -24,3 +24,22 @@ const Btn = () => {
 }
 
 export default Btn;
+
+
+//comparing HTML with React
+
+//HTML
+<button id="js-btn" onclick="clickHandler()"
+>Click me!</button>
+
+//JS Event Handling
+const jsbt = document.getElementById('js-btn')
+jsbt.addEventListener('click', function () {
+  console.log('clicked')
+})
+
+//React
+<button onMouseOver={clickHandler}>
+ Click me!</button>
+//function is not invoked in react
+  
